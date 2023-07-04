7ab491aec557eb03ba03c3302b20fd29f93a2bc8
Babloo Kumar <babloocse888@gmail.com>
2023-07-05 00:38:17 +0530

    :necktie: completed recording process

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let video = document.querySelector("video");
 let captureBtnCont = document.querySelector(".capture-btn-cont");
 let captureBtn = document.querySelector(".capture");
-let recordBtnCont = document.getElementsByClassName("record-btn-cont");
-let recordBtn = document.getElementsByClassName("record-btn");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
 let transparentColor = "transparent";
 let recorder;
 let shouldRecord = false;
 let chunks = [];
 let constraints={
-    video: true,
+    // video: true,
     // audio: true
 }
 navigator.mediaDevices.getUserMedia(constraints)
@@ -17,13 +17,22 @@ navigator.mediaDevices.getUserMedia(constraints)
     recorder = new MediaRecorder(stream);
     recorder.addEventListener("start", () => {
         chunks = [];
+        console.log("recording start");
     })
 
     recorder.addEventListener("dataavailable", (e)=>{
         chunks.push(e.data);
+        console.log("recording push in chunks array");
     })
     recorder.addEventListener("stop", () => {
-
+        let blob = new Blob(chunks, {type:'video/mp4'});
+        console.log("recording stop");
+        let vidUrl = URL.createObjectURL(blob);
+        console.log(vidUrl);
+        let a = document.createElement('a');
+        a.href = vidUrl;
+        a.download = "myVideo.mp4";
+        a.click();
     })
 });
 
@@ -60,3 +69,33 @@ recordBtnCont.addEventListener("click", () => {
         stopTimer();
     }
 })
+
+let timer = document.querySelector(".timer");
+let counter = 0;
+let timerId;
+function startTimer(){
+    timer.style.display = 'block';
+    function displayTimer(){
+        let totalSeconds = counter;
+        let hours = Number.parseInt(totalSeconds / 3600);
+        totalSeconds = totalSeconds % 3600;
+
+        let minutes = Number.parseInt(totalSeconds / 60);
+        totalSeconds = totalSeconds % 60;
+
+        let seconds = totalSeconds;
+
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+        timer.innerText = `${hours}:${minutes}:${seconds}`;
+        counter++;
+    }
+   timerId = setInterval(displayTimer, 1000);
+}
+function stopTimer(){
+    clearInterval(timerId);
+    timer.innerText = "00:00:00";
+    timer.style.display = 'none';
+}
